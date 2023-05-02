96263789ccbe9dcfbc09a4efb461045ccfd2f5de
Isaias Grajales Velazquez <a171008@unach.mx>
2023-05-02 13:46:16 -0600

    html, js modificado y css avanzado

--- funciones.js
+++ funciones.js
@@ -1,13 +1,34 @@
 document.getElementById("enviar").onclick = function(){
-    let temperatura = document.getElementById("temperatura");
-    let respuesta = "";
-    
-    for (var i=0; i<=temperatura.lenght; i++){
-        if(temperatura[i].checked){
-            respuesta = temperatura[i].value;
+    let valorTemperatura = document.querySelector('input[name="temperatura"]:checked').value;
+    let valorTos = document.querySelector('input[name="tos"]:checked').value;
+    let valorDolorCabeza = document.querySelector('input[name="dolor-cabeza"]:checked').value;
+
+    let resultado = "";
+
+    if(valorTemperatura == "si"){
+        if(valorTos == "si"){
+            if(valorDolorCabeza == "si"){
+                resultado = "POSITIVO";
+            }
+            else{
+                resultado = "NEGATIVO";
+            }
+        }
+        else{
+            resultado = "NEGATIVO";
         }
     }
+    else{
+        resultado = "NEGATIVO";
+    }
 
-    console.log(respuesta);
+    console.log(resultado);
+
+    if(resultado == "si"){
+        document.getElementById("positivo").innerHTML = resultado;
+    }
+    else{
+        document.getElementById("negativo").innerHTML = resultado;
+    }
 
 }
