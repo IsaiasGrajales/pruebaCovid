d451e8e6b3c7d952b7d075e4bf044be77d7e7762
Isaias Grajales Velazquez <a171008@unach.mx>
2023-05-03 18:06:04 -0600

    funcion evaluar completado

--- funciones.js
+++ funciones.js
@@ -1,9 +1,21 @@
-document.getElementById("enviar").onclick = function(){
-    let valorTemperatura = document.querySelector('input[name="temperatura"]:checked').value;
-    let valorTos = document.querySelector('input[name="tos"]:checked').value;
-    let valorDolorCabeza = document.querySelector('input[name="dolor-cabeza"]:checked').value;
+var resultado = "";
 
-    let resultado = "";
+function enviarDatos(){
+
+    try {
+        let valorTemperatura = document.querySelector('input[name="temperatura"]:checked').value;
+        let valorTos = document.querySelector('input[name="tos"]:checked').value;
+        let valorDolorCabeza = document.querySelector('input[name="dolor-cabeza"]:checked').value;
+        let prueba = false;
+
+        analizarDatos(valorTemperatura,valorTos,valorDolorCabeza);
+
+    } catch (error) {
+        alert("Es necesario responder las 3 preguntas");
+    }
+}
+
+function analizarDatos(valorTemperatura, valorTos, valorDolorCabeza){
 
     if(valorTemperatura == "si"){
         if(valorTos == "si"){
@@ -22,8 +34,6 @@ document.getElementById("enviar").onclick = function(){
         resultado = "NEGATIVO";
     }
 
-    console.log(resultado);
-
     if(resultado == "si"){
         document.getElementById("positivo").innerHTML = resultado;
     }
@@ -31,4 +41,24 @@ document.getElementById("enviar").onclick = function(){
         document.getElementById("negativo").innerHTML = resultado;
     }
 
+}
+
+function reset(resultado){
+    let radioBotonTemp = document.document.querySelector('input[type=radio][name=temperatura]:checked');
+    let radioButtonTos = document.document.querySelector('input[type=radio][name=tos]:checked');
+    let radioBotonCabeza = document.document.querySelector('input[type=radio][name=dolor-cabeza]:checked');
+
+    radioBotonTemp.checked = false;
+    radioButtonTos.checked = false;
+    radioBotonCabeza.checked = false;
+
+    let aux = resultado;
+    resultado.innerHTML = "";
+
+    if(aux == "POSITIVO"){
+        document.getElementsByName("positivo").innerHTML = resultado;
+    }
+    else{
+        document.getElementsByName("negativo").innerHTML = resultado;
+    }
 }
